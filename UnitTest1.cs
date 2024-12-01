df9fdf691cf70a4bbd4b324d79457d6bce7dc770
SolomiiaMelnyk <116841689+smelnyk26@users.noreply.github.com>
2024-12-01 13:19:15 +0200

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CalcClassBr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcTest { 
+[TestClass]
+public class IABSTests
+{
+        
+    [TestMethod]
+    public void IABS_PositiveNumber_ReturnsNegative()
+    {
+        long a = 123;
+        long expected = -123;
+
+        int result = CalcClass.IABS(a);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void IABS_NegativeNumber_ReturnsPositive()
+    {
+        long a = -456;
+        long expected = 456;
+
+        long result = CalcClass.IABS(a);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void IABS_Zero_ReturnsZero()
+    {
+        long a = 0;
+        long expected = 0;
+
+        long result = CalcClass.IABS(a);
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+    public void IABS_ValueExceedsIntRange_ThrowsException()
+    {
+        long a = (long)int.MaxValue + 1;
+
+        CalcClass.IABS(a);
+    }
+
+       
+    }
+
+
+
+}
